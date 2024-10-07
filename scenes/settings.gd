1c0b1c8a2a6ef64c5ec3f64c37d80c2b7d88b83d
DaltonBaum <dbaum2711@gmail.com>
2024-10-07 11:28:40 -0600

    Walton GUI

--- scenes/settings.gd
+++ scenes/settings.gd
@@ -11,9 +11,8 @@ func _process(delta: float) -> void:
 	pass
 
 
+func _on_rules_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/how_to_play.tscn")
+
 func _on_back_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/main_menu.tscn")
-
-
-func _on_rules_pressed() -> void:
-	get_tree().change_scene_to_file("res://scenes/how_to_play.tscn")
