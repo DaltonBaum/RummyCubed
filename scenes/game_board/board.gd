39c2fd1aa1787f1a6eb738adee11c3104644f2c2
Reformedkoala <93223101+Reformedkoala@users.noreply.github.com>
2024-11-04 18:46:45 -0700

    Changing back to python stuff (painfully)

--- scenes/game_board/board.gd
+++ scenes/game_board/board.gd
@@ -7,20 +7,7 @@ var tile_slot_scene := preload("res://scenes/game_board/tile_slot.tscn")
 
 var invalid_tiles := {}
 var board_valid := false
-var max_tries := 50
-
-enum Colors {
-	RED,
-	BLUE,
-	BLACK,
-	GREEN
-}
-
-enum Relations { 
-	STRAIGHT, 
-	MATCH, 
-	NONE 
-}
+var random_samples := 100
 
 func _ready() -> void:
 	columns = grid_width
@@ -31,14 +18,9 @@ func _ready() -> void:
 		tile_slot.tile_removed.connect(_tile_removed)
 		add_child(tile_slot)
 	
-	var g = create_graph(13, 2, Colors.values())
-	var puzzle = select_tiles(g, 50)
-	var sum = 0
-	for arr in puzzle:
-		for value in arr:
-			sum += 1
-	print(sum)
-	add_tile_groups(puzzle)
+	var g = create_graph(13, 2, TileInfo.Colors.values())
+	var selected_tiles = select_tiles(g, 10)
+	clear_graph(g, selected_tiles)
 
 # Method to call to place starting state on the board
 func add_tile_groups(groups: Array):
@@ -146,7 +128,7 @@ func create_graph(nums: int, decks: int, colors: Array) -> Dictionary:
 		for color in colors:
 			for deck in range(0, decks):
 				var tile = TileInfo.new(num, color, deck)
-				g[tile] = { "relation": Relations.NONE, "connections": [] }
+				g[tile] = { "relation": TileInfo.Relations.INVALID, "connections": {} }
 	connect_graph(g)
 	return g
 
@@ -156,98 +138,151 @@ func connect_graph(g: Dictionary) -> void:
 		for node2 in g.keys():
 			if node.same(node2):
 				continue
-			if node.color == node2.color and abs(node.num - node2.num) == 1 and node.deck == node2.deck:
-				g[node]["connections"].append({"node": node2, "relation": Relations.STRAIGHT})
-			elif node.num == node2.num and node.deck == node2.deck:
-				g[node]["connections"].append({"node": node2, "relation": Relations.MATCH})
+			if node.color == node2.color and abs(node.num - node2.num) == 1:
+				g[node]["connections"][node2] = {"relation": TileInfo.Relations.RUN}
+			elif node.num == node2.num:
+				g[node]["connections"][node2] = {"relation": TileInfo.Relations.SET}
 
-
-# Random selection of tiles and groups to generate valid board state
 func select_tiles(g: Dictionary, count: int) -> Array:
-	var chosen_numbers_decks = []
-	var board = []
-	while count != 0:
-		if count > 3:
-			var match_or_straight = randi_range(0, 1)
-			if match_or_straight == 0:
-				var match_array = []
-				var tile_num = randi_range(1, 13)
-				var deck_num = randi_range(0,1)
-				var random_color = randi_range(0,3)
-				var random_match_size = randi_range(2,3)
-				while ({"tile_num": tile_num, "deck_num": deck_num}) in chosen_numbers_decks:
-					tile_num = randi_range(1, 13)
-					deck_num = randi_range(0,1)
-				chosen_numbers_decks.append({"tile_num": tile_num, "deck_num": deck_num})
-				for tile in g:
-					if tile.num == tile_num && tile.deck == deck_num && tile.color == random_color:
-						for conn in g[tile]["connections"]:
-							if conn["relation"] == Relations.MATCH:
-								match_array.append(conn["node"])
-								if match_array.size() == random_match_size:
-									break
-						match_array.append(tile)
-						break
-				board.append(match_array)
-				count -= random_match_size + 1
-			elif match_or_straight == 1:
-				print("attempting")
-				var straight_array = []
-				var tries = 0
-				var success = false
-				var rand_straight_len = randi_range(3, 12)
-				var rand_color = randi_range(0,3)
-				var tile_num = randi_range(1,13)
-				var deck_num = randi_range(0,1)
-				while success == false:
-					if tries == max_tries:
-						break
-					while ({"tile_num": tile_num, "deck_num": deck_num}) in chosen_numbers_decks:
-						tile_num = randi_range(1, 13)
-						deck_num = randi_range(0,1)
-					for tile in g:
-						if tile.num == tile_num && tile.deck == deck_num && tile.color == rand_color:
-							var conns = []
-							conns.append(tile)
-							for conn in g[tile]["connections"]:
-								if conn["relation"] == Relations.STRAIGHT:
-									if conn["node"].num > tile_num && {"tile_num": conn["node"].num, "deck_num": conn["node"].deck} not in chosen_numbers_decks:
-										conns.append(conn["node"])
-										expand_straight(conn["node"], g, conns, rand_straight_len, chosen_numbers_decks)
-									elif conn["node"].num < tile_num && {"tile_num": conn["node"].num, "deck_num": conn["node"].deck} not in chosen_numbers_decks:
-										conns.push_front(conn["node"])
-										expand_straight(conn["node"], g, conns, rand_straight_len, chosen_numbers_decks)
-							if conns.size() >= rand_straight_len:
-								success = true
-								straight_array = conns
-								break
-							else:
-								break
-					tries += 1
-				if success == true:
-					for node in straight_array:
-						#TODO FIX THIS BECAUSE ITS TECHNICALLY LIMITING POOL TOO MUCH
-						chosen_numbers_decks.append({"tile_num": node.num, "deck_num": node.deck})
-					board.append(straight_array)
-					count -= rand_straight_len
-				else:
-					count -= 3
-		elif count == 3:
-			count -= 3
-		else:
-			count -= 1
-	return board
+	if count < 3:
+		print("Bad Tile count")
+		return []
+	var groups = []
+	groups.append(create_new_group(g))
+	count -=3
+	while count >= 3:
+		groups.append(create_adjacent_group(g))
+		count -=3
+	while count > 0:
+		create_adjacent_tile(g, groups)
+		count -=1
+	var chosen_nodes = []
+	for node in g:
+		if g[node]["relation"] != TileInfo.Relations.INVALID:
+			chosen_nodes.append(node)
+	return chosen_nodes
+	
+func create_new_group(g: Dictionary) -> Array:
+	var nodes = []
+	for node in g:
+		nodes.append(node)
+	for i in range(random_samples):
+		var node1 = nodes[randi() % nodes.size()]
+		var connected_nodes = []
+		for conn in g[node1]["connections"].keys():
+			if conn in nodes:
+				connected_nodes.append(conn)
+		if connected_nodes.size() == 0:
+			continue
+		var node2 = connected_nodes[randi() % connected_nodes.size()]
+		var relation = g[node1]["connections"][node2]["relation"]
+		connected_nodes = []
+		for conn in g[node1]["connections"].keys():
+			if conn in nodes and not node1.same(conn) and g[node1]["connections"][conn]["relation"] == relation:
+				connected_nodes.append(conn)
+		for conn in g[node2]["connections"].keys():
+			if conn in nodes and not node2.same(conn) and  g[node2]["connections"][conn]["relation"] == relation:
+				connected_nodes.append(conn)
+		if connected_nodes.size() == 0:
+			continue
+		var node3 = connected_nodes[randi() % connected_nodes.size()]
+		g[node1]["relation"] = relation
+		g[node2]["relation"] = relation
+		g[node3]["relation"] = relation
+		return [node1, node2, node3]
+	print("failed to make new group")
+	return []
+	
+func create_adjacent_group(g: Dictionary) -> Array:
+	var used_nodes = []
+	var possible_nodes = []
+	for node in g:
+		if g[node]["relation"] != TileInfo.Relations.INVALID:
+			used_nodes.append(node)
+	for node in used_nodes:
+		for conn in g[node]["connections"].keys():
+			if conn not in used_nodes:
+				possible_nodes.append(conn)
+	if possible_nodes.size() == 0:
+		print("failed to make adjacent group")
+		return []
+	for i in range(random_samples):
+		var node1 = possible_nodes[randi() % possible_nodes.size()]
+		var connected_nodes = []
+		for conn in g[node1]["connections"].keys():
+			if conn not in used_nodes:
+				connected_nodes.append(conn)
+		if connected_nodes.size() == 0:
+			continue
+		var node2 = connected_nodes[randi() % connected_nodes.size()]
+		var relation = g[node1]["connections"][node2]["relation"]
+		connected_nodes = []
+		for conn in g[node1]["connections"].keys():
+			if conn not in used_nodes and not node1.same(conn) and g[node1]["connections"][conn]["relation"] == relation:
+				connected_nodes.append(conn)
+		for conn in g[node2]["connections"].keys():
+			if conn not in used_nodes and not node2.same(conn) and  g[node2]["connections"][conn]["relation"] == relation:
+				connected_nodes.append(conn)
+		if connected_nodes.size() == 0:
+			continue
+		var node3 = connected_nodes[randi() % connected_nodes.size()]
+		g[node1]["relation"] = relation
+		g[node2]["relation"] = relation
+		g[node3]["relation"] = relation
+		return [node1, node2, node3]
+	print("failed to make adjacent group")
+	return []
 
-func expand_straight(node: Object, g: Dictionary, total_conns: Array, max: int,  chosen_numbers_decks: Array) -> void:
-	if total_conns.size() > max:
+func create_adjacent_tile(g: Dictionary, groups: Array) -> void:
+	var used_nodes = []
+	for node in g:
+		if g[node]["relation"] != TileInfo.Relations.INVALID:
+			used_nodes.append(node)
+	for i in range(random_samples):
+		var nodes = groups[randi() % groups.size()]
+		var relation = g[nodes[0]]["relation"]
+		var conns = []
+		for conn in g[nodes[0]]["connections"].keys():
+			if conn not in used_nodes and not nodes[0].same(conn) and g[nodes[0]]["connections"][conn]["relation"] == relation:
+				conns.append(conn)
+		for conn in g[nodes[1]]["connections"].keys():
+			if conn not in used_nodes and not nodes[1].same(conn) and g[nodes[1]]["connections"][conn]["relation"] == relation:
+				conns.append(conn)
+		for conn in g[nodes[2]]["connections"].keys():
+			if conn not in used_nodes and not nodes[2].same(conn) and g[nodes[2]]["connections"][conn]["relation"] == relation:
+				conns.append(conn)
+		if conns.size() == 0:
+			print("no adjacent tiles")
+			continue
+		var node = conns[randi() % conns.size()]
+		g[node]["relation"] = relation
+		nodes.append(node)
 		return
-	for inner_node in g[node]["connections"]:
-		if inner_node["relation"] == Relations.STRAIGHT && inner_node["node"] not in total_conns:
-			if inner_node["node"].num < node.num && {"tile_num": inner_node["node"].num, "deck_num": inner_node["node"].deck} not in chosen_numbers_decks:
-				total_conns.push_front(inner_node["node"])
-				print("hello in recursion 1")
-				expand_straight(inner_node["node"], g, total_conns, max, chosen_numbers_decks)
-			if inner_node["node"].num > node.num && {"tile_num": inner_node["node"].num, "deck_num": inner_node["node"].deck} not in chosen_numbers_decks:
-				total_conns.append(inner_node["node"])
-				print("hello in recursion 2")
-				expand_straight(inner_node["node"], g, total_conns, max, chosen_numbers_decks)
+	print("failed to add adjacent tile")
+	return 
+	
+func clear_graph(og: Dictionary, selected_tiles: Array) -> void:
+	for node in og.keys():
+		for conn in og[node]["connections"].keys():
+			if conn not in selected_tiles:
+				og[node]["connections"].erase(conn)
+		if node not in selected_tiles:
+			og.erase(node)
+		else:
+			og[node]["relation"] = TileInfo.Relations.INVALID
+	for start in og:
+		var groups = {}
+		for r in range(3):
+			groups[r] = {}
+		var counts = {}
+		for r in range(3):
+			counts[r] = 0
+		for conn in og[start]["connections"].keys():
+			if not groups[og[start]["connections"][conn]["relation"]].has(conn):
+				counts[og[start]["connections"][conn]["relation"]] += 1
+			groups[og[start]["connections"][conn]["relation"]][conn] = true
+		for relation in range(3):
+			if counts[relation] < 2:
+				for end in groups[relation].keys():
+					og.erase(end)
+					og[start]["connections"].erase(end)
