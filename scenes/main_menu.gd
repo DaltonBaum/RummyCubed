2768b074e18f1b360b46b9714f8f127033797295
Austin Damek <austindamek@mines.edu>
2024-11-13 09:31:51 -0700

    Moved how to play button out of settings

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -13,5 +13,9 @@ func _on_settings_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/settings.tscn")
 
 
+func _on_how_to_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/how_to_play.tscn")
+
+
 func _on_credits_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/credits.tscn")
--- scenes/pause_menu.gd
+++ scenes/pause_menu.gd
@@ -5,6 +5,10 @@ func _on_settings_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/settings.tscn")
 
 
+func _on_how_to_play_button_pressed() -> void:
+	get_tree().change_scene_to_file("res://scenes/how_to_play.tscn")
+
+
 func _on_restart_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/game_board/game.tscn")
 
