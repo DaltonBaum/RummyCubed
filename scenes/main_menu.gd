c9142b9aaf895e66a5d159a5e8d76631a81bfb3a
Austin Damek <austindamek@mines.edu>
2024-12-01 17:22:22 -0700

    Made daily puzzle a medium instead of hard

--- scenes/main_menu.gd
+++ scenes/main_menu.gd
@@ -9,7 +9,7 @@ func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/difficulty_menu.tscn")
 
 func _on_daily_button_pressed() -> void:
-	PuzzleInfo.start_game(45, 60, Time.get_date_string_from_system())
+	PuzzleInfo.start_game(30, 45, Time.get_date_string_from_system())
 
 func _on_settings_button_pressed() -> void:
 	_hide_menu()
